032c51f5de35d39c776929c335d8d0cd1284a293
Abhyuday <abhyuday911@gmail.com>
2025-11-07 14:25:31 +0530

    feat: basic async actix http-server from docs

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, Result, get, post, web};
+use serde::Serialize;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[derive(Serialize)]
+struct MyObj {
+    name: String,
+}
+
+#[get("/a/{name}")]
+async fn index(name: web::Path<String>) -> Result<impl Responder> {
+    let obj = MyObj {
+        name: name.to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .service(index)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
